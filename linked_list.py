402fe332ccaf44667741aa2157d63537b9ce53c6
Yusuf Cattaneo <ycattaneo@moveworks.ai>
2021-12-26 17:10:42 -0600

    Creating Base Files

---
+++ linked_list.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.last_node = None 
